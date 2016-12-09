e32837d2d1236f4c33e44087ddbc3ad465532dd5
Dana Kaplan <danamkaplan@gmail.com>
2016-12-09 14:41:49 -0800

    hmmm

--- Clean.py
+++ Clean.py
@@ -1,10 +1,21 @@
+import pandas as pd
+import numpy as np
 #400,000 rows
 #5,000 model ID
 
 
 def clean(df):
     df['Age'] = 2016 - df['YearMade'] #this creates the Age column, current year (2016) - Year Made
+    df = df[df['Age'] < 900] #drop less than 1000
+    #df['Coupler_System'] = pd.get_dummies(df['Coupler_System'])['Yes']
 
-    df['Coupler_System'] = pd.get_dummies(df['Coupler_System'])['Yes']
 
-    return (clean_df)
+
+    #creates categorical values for 6 different product groups
+    #sets baseline of variable 'TEX' as our most frequently sold type
+    prod_groups = pd.get_dummies(df['ProductGroup']).drop('TEX', axis=1)
+    #adds variables to end of dataframe
+    df = pd.concat([df, prod_groups], axis=1)
+
+    return (df)
+
--- model.py
+++ model.py
@@ -1,13 +1,45 @@
 import pandas as pd
+import statsmodels.api as sm
 import Clean
 
 
-import pandas as pd
 low_memory=False
 
 
 df = pd.read_csv('unzipped_data/Train.csv')
+testdf = pd.read_csv('./unzipped_data/test.csv')
 
 df = Clean.clean(df)
+testdf = Clean.clean(testdf)
 
-#modeling stuff with df
+#model one
+X_train = df[['WL','SSL','BL','TTT','MG']]
+y_train = df['SalePrice']
+X_train = sm.add_constant(X_train)
+
+model = sm.OLS(y_train, X_train)
+result = model.fit()
+
+X_test = testdf[['WL','SSL','BL','TTT','MG']]
+X_test = sm.add_constant(X_test)
+
+testdf['SalePrice'] = result.predict(X_test)
+testdf[['SalesID', 'SalePrice']].to_csv('model1.csv', index=False, sep=',')
+
+
+#model2 with age
+features = ['WL','SSL','BL','TTT','MG','Age']
+X_train = df[features]
+X_test = sm.add_constant(X_test)
+
+model2 = sm.OLS(y_train, X_train)
+result2 = model2.fit()
+X_test = testdf[features]
+
+
+testdf['SalePrice'] = result.predict(X_test)
+testdf[['SalesID', 'SalePrice']].to_csv('model2.csv', index=False, sep=',')
+
+
+
+
