b2b8c09a353be4cdaf130b226ab152ec866e2a85
Dana Kaplan <danamkaplan@gmail.com>
2016-12-09 11:40:24 -0800

    coupler system is a bool

--- Clean.py
+++ Clean.py
@@ -5,7 +5,6 @@
 def clean(df):
     df['Age'] = 2016 - df['YearMade'] #this creates the Age column, current year (2016) - Year Made
 
-
-
+    df['Couple_System'] = pd.get_dummies(df['Coupler_System'])['Yes']
 
     return (clean_df)
